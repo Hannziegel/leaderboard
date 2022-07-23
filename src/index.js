3e4e8a22b472ff06488cb28f8ca6cd04345322ed
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-07-23 19:44:59 -0300

    Deleted unused imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import './style.css';
 import { nameInput, scoreInput, submitInputButton, refreshButton } from './modules/htmlConsts';
 import { removeAllChildHTML } from './modules/pageFunctions.js';
-import { setLocalStorage, checkLocalStorage, setLocalStorageID, getLocalStorageID, checkLocalStorageID, getLocalStorageArr } from './modules/localStorageFunctions.js';
+import { setLocalStorage, checkLocalStorage, getLocalStorageID, checkLocalStorageID } from './modules/localStorageFunctions.js';
 import { postScore, getScores, createGame } from './modules/leaderboardApi';
 import Scores from './modules/Scores.js';
 
