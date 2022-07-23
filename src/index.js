186786afc9e254df8cf8c060591e1f9f1135ce9b
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-07-23 20:27:58 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 import {
-          nameInput, scoreInput, submitInputButton, refreshButton
-        } from './modules/htmlConsts.js';
+  nameInput, scoreInput, submitInputButton, refreshButton,
+} from './modules/htmlConsts.js';
 import { removeAllChildHTML } from './modules/pageFunctions.js';
 import {
-          setLocalStorage, checkLocalStorage, getLocalStorageID, checkLocalStorageID
-        } from './modules/localStorageFunctions.js';
+  setLocalStorage, checkLocalStorage, getLocalStorageID, checkLocalStorageID,
+} from './modules/localStorageFunctions.js';
 import { postScore, getScores, createGame } from './modules/leaderboardApi.js';
 import Scores from './modules/Scores.js';
 
